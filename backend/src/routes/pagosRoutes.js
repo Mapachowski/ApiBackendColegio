124d7d7c9a56979d14ae3a0494d76df775137ed5
jedf15 <jdeyet@gmail.com>
2025-11-10 06:01:35 +0000

    Actualización en backend - ajustes recientes pagos

--- backend/src/routes/pagosRoutes.js
+++ backend/src/routes/pagosRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const pagosController = require('../controllers/pagosController'); // ✅ sube un nivel
 
 // 🔹 Rutas para pagos
-router.get('/mesesPagados/:idAlumno/:tipoPago/:cicloEscolar', pagosController.getMesesPagados);
+router.get('/meses-pagados/:idAlumno/:tipoPago/:cicloEscolar', pagosController.getMesesPagados);
 router.get('/', pagosController.getAll);
 router.get('/:id', pagosController.getById);
 router.get('/numero/:numeroRecibo', pagosController.getByNumeroRecibo);
